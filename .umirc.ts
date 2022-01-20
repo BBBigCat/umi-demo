37634d568276f6e8ea72d4322cbd0c5166b34e2c
bin.liu <bin.liu@yuanzhanghu.com>
2022-01-20 11:13:49 +0800

    chore: 增加 define 变量

--- .umirc.ts
+++ .umirc.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'umi';
-
 export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
   routes: [{ path: '/', component: '@/pages/index' }],
-  // fastRefresh: {},
+  define: {
+    'process.env.__DEV__': process.env.dev,
+  },
   webpack5: {},
 });
